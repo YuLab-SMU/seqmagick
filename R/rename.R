182cd622ff8a998c8c9d0c457d396cdf73cc2abc
guangchuang yu <guangchuangyu@gamil.com>
2016-11-30 17:49:09 +0800

    bug fixed

--- R/rename.R
+++ R/rename.R
@@ -1,6 +1,6 @@
 ##' rename fasta sequence name
 ##'
-##' 
+##'
 ##' @title fa_rename
 ##' @param fasfile fasta file
 ##' @param mapping_file mapping file
@@ -24,7 +24,7 @@ fa_rename <- function(fasfile, mapping_file, position, sep, mode, outfile) {
 
 ##' rename sequence
 ##'
-##' 
+##'
 ##' @title bs_rename
 ##' @param x BStringSet object
 ##' @param mapping two column data.frame
@@ -36,23 +36,23 @@ fa_rename <- function(fasfile, mapping_file, position, sep, mode, outfile) {
 ##' @author Guangchuang Yu
 bs_rename <- function(x, mapping, position, sep, mode) {
     mode <- match.arg(mode, c("replace", "prefix", "suffix"))
-    
+
     nn <- names(x)
-    
+
     if (missing(sep)) {
         id <- names(x)
     } else {
         id <- sapply(tokenize(nn, sep), function(y) y[1])
     }
-    
+
     if (!missing(position)) {
         if (missing(sep))
             stop("separator should be provided")
-        
+
         tk <- sapply(tokenize(nn, sep), function(y) y[position])
-        
+
         i <- match(id, mapping[,1])
-        
+
         if (mode == "replace") {
             to <- mapping[i,2]
         } else if (mode == "prefix") {
@@ -60,15 +60,15 @@ bs_rename <- function(x, mapping, position, sep, mode) {
         } else {
             to <- paste(tk, mapping[i, 2], sep="")
         }
-        
+
         tks <- tokenize(nn, sep)
         tks <- lapply(seq_along(tks), function(i) {
             tks[[i]][position] <- to[i]
             tks[[i]]
         })
-        
+
         id2 <- sapply(tks, paste0, collapse=sep)
-        
+
     } else {
         if (mode == "replace") {
             i <- match(id, mapping[,1])
@@ -77,10 +77,14 @@ bs_rename <- function(x, mapping, position, sep, mode) {
             id2 <- paste(mapping[i,2], id, sep="")
         } else {
             id2 <- paste(id, mapping[i, 2], sep="")
-        
         }
     }
-    
+    id2 <- as.character(id2)
+    ii <- is.na(id2)
+    if (any(ii)) {
+        message(sum(ii), " sequences failed to rename, original name will be kept...")
+        id2[ii] <- names(x)[ii]
+    }
     names(x) <- id2
     return(x)
 }
