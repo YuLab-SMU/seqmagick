9b166bc46d64cda79c834c6f5cdc39f2b2854614
Guangchuang Yu <guangchuangyu@gmail.com>
2026-01-31 15:39:40 +0800

    use yulab.utils:::mydownload

--- R/NCBI.R
+++ R/NCBI.R
@@ -6,7 +6,6 @@
 ##' @param db supported db, currently 'nuccore'
 ##' @param format one of 'genbank' or 'fasta'
 ##' @param outfile output file, by default, acc.gb or acc.fa
-##' @param ... additional parameters for download.file
 ##' @return output file vector
 ##' @importFrom magrittr %>%
 ##' @export
@@ -18,7 +17,7 @@
 ##' download_genbank(acc='AB115403', format='genbank', outfile=tmpgb)
 ##' download_genbank(acc='AB115403', format='fasta', outfile=tmpfa)
 ##' }
-download_genbank <- function(acc, db="nuccore", format = "genbank", outfile=NULL, ...) {
+download_genbank <- function(acc, db="nuccore", format = "genbank", outfile=NULL) {
     if (!is.null(outfile) && (length(outfile) != length(acc))) {
         stop("'outfile' & 'acc' should be equal length...")
     }
@@ -31,13 +30,13 @@ download_genbank <- function(acc, db="nuccore", format = "genbank", outfile=NULL
         } else {
             of <- outfile[i]
         }
-        ofs[i] <- download_genbank_item(acc[i], db, format, of, ...)
+        ofs[i] <- download_genbank_item(acc[i], db, format, of)
     }
     invisible(ofs)
 }
 
 ##' @importFrom utils download.file
-download_genbank_item <- function(acc, db="nuccore", format = "genbank", outfile=NULL, ...) {
+download_genbank_item <- function(acc, db="nuccore", format = "genbank", outfile=NULL) {
     format <- match.arg(format, c('genbank', 'fasta'))
     if (db != "nuccore") {
         stop("currently, only nuccore is supported...")
@@ -52,7 +51,7 @@ download_genbank_item <- function(acc, db="nuccore", format = "genbank", outfile
         outfile <- paste0(acc, suffix)
     }
     
-    downloader::download(url=url, destfile = outfile,  ...)
+    yulab.utils:::mydownload(url=url, destfile = outfile)
     invisible(outfile)
 }
 
@@ -63,7 +62,6 @@ download_genbank_item <- function(acc, db="nuccore", format = "genbank", outfile
 ##' @param acc accession number(s)
 ##' @param db supported db, currently 'nuccore'
 ##' @param type one of 'DNA', 'RNA', 'AA', 'unknown' or 'auto'
-##' @param ... additional parameters passed to downloader::download
 ##' @return BStringSet object
 ##' @export
 ##' @author Guangchuang Yu
@@ -72,7 +70,7 @@ download_genbank_item <- function(acc, db="nuccore", format = "genbank", outfile
 ##' x <- ncbi_fa_read(c('AB115403', 'AB115403.1'))
 ##' x
 ##' }
-ncbi_fa_read <- function(acc, db="nuccore", type = "auto", ...) {
+ncbi_fa_read <- function(acc, db="nuccore", type = "auto") {
     if (length(acc) == 0) {
         stop("'acc' is empty...")
     }
@@ -80,26 +78,23 @@ ncbi_fa_read <- function(acc, db="nuccore", type = "auto", ...) {
         stop("'acc' contains NA...")
     }
 
+    if (db != "nuccore") {
+        stop("currently, only nuccore is supported...")
+    }
+
     acc <- as.character(acc)
     acc <- gsub("\\.\\d+", "", acc)
 
-    tmpdir <- tempfile("seqmagick-ncbi-")
-    dir.create(tmpdir)
-    on.exit(unlink(tmpdir, recursive=TRUE, force=TRUE), add=TRUE)
-
-    destfile <- file.path(tmpdir, paste0(make.unique(acc), ".fa"))
-    download_genbank(acc, db=db, format="fasta", outfile=destfile, ...)
+    acc_query <- paste0(acc, collapse=",")
+    url <- paste0(
+        "https://www.ncbi.nlm.nih.gov/sviewer/viewer.cgi?tool=portal&sendto=on&log$=seqview&db=",
+        db,
+        "&dopt=fasta&sort=&val=",
+        acc_query
+    )
 
     f <- tempfile(fileext = ".fasta")
-    for (i in seq_along(destfile)) {
-        x <- readLines(destfile[i], warn=FALSE)
-        if (length(x) == 0) {
-            next
-        }
-        cat(x, file = f, append = TRUE, sep="\n")
-        cat("\n", file = f, append = TRUE)
-    }
-
+    yulab.utils:::mydownload(url=url, destfile=f)
     fa_read(f, type=type)
 }
 
